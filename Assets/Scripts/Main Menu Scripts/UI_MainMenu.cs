f9697d9e79f0e92be3de1b44b5462d757a50980d
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2024-06-26 11:27:26 +0200

    Import TMP

---
+++ Assets/Scripts/Main Menu Scripts/UI_MainMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+
+public class UI_MainMenu : MonoBehaviour
+{
+    public NetworkManager networkManager;
+    public UnityTransport transport;
+    public GameObject MenuCanvas;
+    [Space(10)]
+    public GameObject ipWindow;
+    public Text ipField;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BTN_StartHost()
+    {
+        networkManager.StartHost();
+    }
+    public void BTN_JoinGame()
+    {
+        networkManager.StartClient();
+    }
+
+    public void BTN_Join()
+    {
+        transport.ConnectionData.
+    }
+}
