1e14bdd006c2425e4d33ee7a3d8bd60abe62298a
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2024-07-05 18:48:46 +0200

    Add Stats

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public float healthMax;
+    public float health;
+    public float healthRegen;
+    public float healthDelay;
+
+    public float staminaMax;
+    public float stamina;
+    public float staminaRegen;
+    public float staminaDelay;
+
+    //public float movementSpeed;
+}
