3f7555072279b6fc6318e7d5f70e86e75aca6081
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2024-09-10 16:02:12 +0200

    Enemy AI Start

---
+++ Assets/Scripts/Enemy/Enemy_AI_Melee.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_AI_Melee : MonoBehaviour
+{
+    [SerializeField] float attackRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
