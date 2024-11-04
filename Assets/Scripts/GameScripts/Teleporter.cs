5db1a6dacc9b6f7a3b5b8c399a5789d17940844b
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2024-11-04 14:33:59 +0100

    Add Brick Walls

---
+++ Assets/Scripts/GameScripts/Teleporter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : InteractableBase
+{
+    public Transform teleportPosition;
+    public RoomManager interactedRoom;
+
+    public override void Interact(PlayerData data)
+    {
+        // Activate Room
+        // Teleport Player
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
