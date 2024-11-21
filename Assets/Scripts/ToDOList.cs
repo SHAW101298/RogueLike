017de878f30bba12d2e92c0013a002e7fe68faca
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2024-11-21 16:51:00 +0100

    Update ToDOList.cs

--- Assets/Scripts/ToDOList.cs
+++ Assets/Scripts/ToDOList.cs
@@ -34,6 +34,7 @@ public class ToDOList : MonoBehaviour
         doZrobienia.Add("Ręce również powinny się obracać wraz z obrotem kamery");
         // https://www.youtube.com/watch?v=luBBz5oeR4Q
         doZrobienia.Add("Loadign Screen");
+        doZrobienia.Add("Przechowanie broni na czas zmiany postaci");
     }
 
 }
