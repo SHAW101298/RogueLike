150df71e24c9c0192588c42cb3991eba1212b318
SHAW101298 <59138549+SHAW101298@users.noreply.github.com>
2025-02-03 13:30:01 +0100

    Added Map Manger

---
+++ Assets/Scripts/GameScripts/MapManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public RoomManager Spawn;
+    public GameObject mapParent;
+    public List<RoomManager> floorSpawns;
+    public List<GameObject> floorParents;
+
+
+    public void ActivateFloor(int floorNumber)
+    {
+        
+    }
+}
